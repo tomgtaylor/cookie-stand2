ceceaf6d9dad21bbf4507edff7753940edcac66a
Tom Taylor <tomgtaylor@yahoo.com>
2022-06-17 00:52:42 -0700

    added table, image, and data into Sales page

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 'use strict';
 
-let openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+let openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
 let salesTable = document.getElementById('cookieResults');
 //-----------------------------------------------
-//let theTotal = 0;
-
-// constructor function passing parameters for (name, min, max, avg).
-    // instide we have objects initialized in instances of class "this" (this)
+// constructor function passing parameters for ( name, min, max, avg ).
+    // instide we have objects initialized in instances of class "this" ( this )
     // "this" is like a bookmark indicating the page. The object "name" is the page in the book.
     // constructor function is the book with objects. 
 //-----------------------------------------------
-function CookieShop (name, min, max, average){     // constructor that groups variables like name, min, max, etc..
+function CookieStore (name, min, max, average){     // constructor that groups variables like name, min, max, etc..
     this.name = name;
     this.min = min;
     this.max = max;
@@ -19,58 +17,66 @@ function CookieShop (name, min, max, average){     // constructor that groups va
     this.cookiesTotal = 0;
     this.calcCookiesPerHour();
 
-    CookieShop.all.push(this);      //pushes all (everything) into this.
-};
+    CookieStore.all.push(this);      //pushes all (everything) into this.
+}
 //-----------------------------------------------
-CookieShop.all = [];    //stores all(everything) in an array. Data is not organized/arranged yet.
+CookieStore.all = [];    //stores all(everything) in an array. Data is not organized/arranged yet.
 // openHours.push(this);
 
-CookieShop.prototype.calcCookiesPerHour = function () {  //prototype inherits features from the constructor
-    for (let i = o; i< openHours.length; i++){
-        let hourlyCookies = math.ceil(this.calcCustomersPerHour() * this.average );
+CookieStore.prototype.calcCookiesPerHour = function() {  //prototype inherits features from the constructor
+    for (let i = 0; i < openHours.length - 1; i++) {
+        let hourlyCookies = Math.ceil(this.randomNumGen() * this.average);
         this.cookiesPerHour.push(hourlyCookies);
         this.cookiesTotal += hourlyCookies;
     }
 };
 //-----------------------------------------------
-CookieShop.prototype.calcCookiesPerHour = function () {
-    return Math.round(Math.random() * (this.max - this.min)) + this.min;
+CookieStore.prototype.randomNumGen = function() {
+    return Math.round(Math.random() * (this.max - this.min + 1)) + this.min;
 };
-CookieShop.prototype.renderStoreRow = function() {
+//-----------------------------------------------
+CookieStore.prototype.renderStoreRow = function() {
     let storeRow = document.createElement('tr');
     salesTable.appendChild(storeRow);
     let storeName = document.createElement('td');
     storeName.textContent = this.name;
     storeRow.appendChild(storeName);
-    for (let i = 0; i < 15; i++); {
-        let tableElement = document.createElement('tr');
-        tableElement.textContent = openHours;
-        storeRow.appendChild(tableElement);
+
+    for (let i = 0; i < openHours - 1; i++); {
+        let tableRowElement = document.createElement('tr');
+        tableRowElement.textContent = this.cookiesPerHour;
+        storeRow.appendChild(tableRowElement);
     }
 };
 //----------------------------------------------------
-let seattle = new CookieShop('Seattle', 23, 65, 6.3);
-let tokyo = new CookieShop('Tokyo', 3, 24, 1.2);
-let dubai = new CookieShop('Dubai', 11, 38, 3.7);
-let paris = new CookieShop('Paris', 20, 38, 2.3);
-let lima = new CookieShop('Lima', 2, 16, 4.6);
-//-----------------------------------------------
-// console.log(CookieShop.Seattle);
 
-//     render what's in the app.js into HTML file, render what is in the console.log
+let seattle = new CookieStore('Seattle', 23, 65, 6.3);
+let tokyo = new CookieStore('Tokyo', 3, 24, 1.2);
+let dubai = new CookieStore('Dubai', 11, 38, 3.7);
+let paris = new CookieStore('Paris', 20, 38, 2.3);
+let lima = new CookieStore('Lima', 2, 16, 4.6);
+
+console.log(CookieStore.all);
+console.log(CookieStore.all[0].cookiesPerHour[1]);
+
+//-----------------------------------------------
+// console.log(CookieStore.Seattle);
+
+//     render what's in the app.js into HTML file, render what is in the console.log. (worked on with Kassie and Trevor)
 //     target your HTML elements by their id type. For example:
 //     Hours element, would get the hours array, and render it
 function renderHeaderRow(){
-    const tableHeaderRow = document.createElement('tr');
-    salesTable.appendChild(tableHeaderRow);    // glue tableHeaderRow to the table
-    let emptyCell = document.createElement('td');
-    tableHeaderRow.appendChild(emptyCell);
-    for (let i = 0; i < 15; i++) {
-        let tableHeader = document.createElement('td'); 
-        tableHeader.textContent = openHours[i];
-        tableHeaderRow.appendChild(tableHeader);
-    }
-    getSalesTable.appendChild(tableHeaderRow);
+    let tableHeaderRow = document.createElement('tr');
+    let theTotal = document.createElement('td');
+    salesTable.appendChild(theTotal);    // glue "theTotal" to the table
+
+        for (let i = 0; i < openHours.length; i++) {
+
+            let tableHeader = document.createElement('td'); 
+            tableHeader.textContent = openHours[i];
+            tableHeaderRow.appendChild(tableHeader);
+        }
+        salesTable.appendChild(tableHeaderRow);
 };
 //-----------------------------------------------
 function renderFooterRow(){
@@ -78,12 +84,17 @@ function renderFooterRow(){
     salesTable.appendChild(tableFooterRow);    // glue tableHeaderRow to the table
     let emptyCell = document.createElement('td');
     tableFooterRow.appendChild(emptyCell);
+        // This should give us each hour for each location, and addes all the numbers.
     for (let i = 0; i < 15; i++) {
+        let sum = 0;
+        for (let x = 0; x < CookieStore.all.length; X++);{
+        sum += CookieStore.all[x].cookiesPerHour[i];
+        }
         let tableFooter = document.createElement('td'); 
-        tableFooter.textContent = openHours[i];
+        tableFooter.textContent = sum;
         tableFooterRow.appendChild(tableFooter);
     }
-    getSalesTable.appendChild(tableFooterRow);
+    salesTable.appendChild(tableFooterRow);
 };
 //-----------------------------------------------
 renderHeaderRow();
@@ -107,6 +118,6 @@ renderFooterRow();
 // tableRowEl.appendChild(nameEl);
 // tableRowEl.appendChild(hpEl);
 
-// nameEl.textContent = CookieShop.name;
-// hpEl.textContent = CookieShop.hp;
+// nameEl.textContent = CookieStore.name;
+// hpEl.textContent = CookieStore.hp;
 
