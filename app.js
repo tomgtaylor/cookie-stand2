917b1777f109a310d5b22218c03e5f7f31a743c3
Tom Taylor <tomgtaylor@yahoo.com>
2022-06-16 20:51:49 -0700

    problems with code

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
 let openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 let salesTable = document.getElementById('cookieResults');
-
+//-----------------------------------------------
 //let theTotal = 0;
 
 // constructor function passing parameters for (name, min, max, avg).
     // instide we have objects initialized in instances of class "this" (this)
     // "this" is like a bookmark indicating the page. The object "name" is the page in the book.
     // constructor function is the book with objects. 
-
+//-----------------------------------------------
 function CookieShop (name, min, max, average){     // constructor that groups variables like name, min, max, etc..
     this.name = name;
     this.min = min;
@@ -21,18 +21,18 @@ function CookieShop (name, min, max, average){     // constructor that groups va
 
     CookieShop.all.push(this);      //pushes all (everything) into this.
 };
-
+//-----------------------------------------------
 CookieShop.all = [];    //stores all(everything) in an array. Data is not organized/arranged yet.
-openHours.push(this);
+// openHours.push(this);
 
 CookieShop.prototype.calcCookiesPerHour = function () {  //prototype inherits features from the constructor
     for (let i = o; i< openHours.length; i++){
         let hourlyCookies = math.ceil(this.calcCustomersPerHour() * this.average );
         this.cookiesPerHour.push(hourlyCookies);
-        this.cookieTotal += hourlyCookies;
+        this.cookiesTotal += hourlyCookies;
     }
 };
-
+//-----------------------------------------------
 CookieShop.prototype.calcCookiesPerHour = function () {
     return Math.round(Math.random() * (this.max - this.min)) + this.min;
 };
@@ -40,11 +40,13 @@ CookieShop.prototype.renderStoreRow = function() {
     let storeRow = document.createElement('tr');
     salesTable.appendChild(storeRow);
     let storeName = document.createElement('td');
-    storeRow.appendChild(storeName);
     storeName.textContent = this.name;
-    // for (let i = 0; i< 15; i++) {
-        // work on this..................
-    // } 
+    storeRow.appendChild(storeName);
+    for (let i = 0; i < 15; i++); {
+        let tableElement = document.createElement('tr');
+        tableElement.textContent = openHours;
+        storeRow.appendChild(tableElement);
+    }
 };
 //----------------------------------------------------
 let seattle = new CookieShop('Seattle', 23, 65, 6.3);
@@ -52,7 +54,7 @@ let tokyo = new CookieShop('Tokyo', 3, 24, 1.2);
 let dubai = new CookieShop('Dubai', 11, 38, 3.7);
 let paris = new CookieShop('Paris', 20, 38, 2.3);
 let lima = new CookieShop('Lima', 2, 16, 4.6);
-
+//-----------------------------------------------
 // console.log(CookieShop.Seattle);
 
 //     render what's in the app.js into HTML file, render what is in the console.log
@@ -68,15 +70,32 @@ function renderHeaderRow(){
         tableHeader.textContent = openHours[i];
         tableHeaderRow.appendChild(tableHeader);
     }
+    getSalesTable.appendChild(tableHeaderRow);
 };
-
+//-----------------------------------------------
+function renderFooterRow(){
+    const tableFooterRow = document.createElement('tr');
+    salesTable.appendChild(tableFooterRow);    // glue tableHeaderRow to the table
+    let emptyCell = document.createElement('td');
+    tableFooterRow.appendChild(emptyCell);
+    for (let i = 0; i < 15; i++) {
+        let tableFooter = document.createElement('td'); 
+        tableFooter.textContent = openHours[i];
+        tableFooterRow.appendChild(tableFooter);
+    }
+    getSalesTable.appendChild(tableFooterRow);
+};
+//-----------------------------------------------
 renderHeaderRow();
+
 seattle.renderStoreRow();
 tokyo.renderStoreRow();
 dubai.renderStoreRow();
 paris.renderStoreRow();
 lima.renderStoreRow();
 
+renderFooterRow();
+//-----------------------------------------------
 
 // add a table row to our table element
 // let tableEl = document.getElementById('cookieResults');
